bb189765231aad9a39314d87949ff6df101fdeaa
XCODEJET <117024606+xcodejet@users.noreply.github.com>
2023-05-08 20:58:44 +0530

    Delete kumuthu.js

--- kumuthu.js
+++
@@ -1,3 +0,0 @@
-console.log("")
-console.log("%c\nU.V.D.Kumuthu Prabhasha","color: red; font-size: 20px; background-image: linear-gradient(#00ff00,yellow); padding: 20px; border-radius: 50px; border: 3px solid white; font-weight: bolder;")
-console.log("")
