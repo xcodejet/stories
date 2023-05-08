c91c3081a920e955f7f24a89316c8ec93924148f
XCODEJET <117024606+xcodejet@users.noreply.github.com>
2023-05-08 20:59:21 +0530

    ඉන්ඩෙක්ස්

---
+++ kumuthu.js
@@ -0,0 +1,3 @@
+console.log("")
+console.log("%c\nU.V.D.Kumuthu Prabhasha","color: red; font-size: 20px; background-image: linear-gradient(#00ff00,yellow); padding: 20px; border-radius: 50px; border: 3px solid white; font-weight: bolder;")
+console.log("")
